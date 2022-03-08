220374c40c2ae0499bb6c7faea9fb4007880dd84
JinSeong970419 <73704533+JinSeong970419@users.noreply.github.com>
2022-03-08 23:31:49 +0900

    CurveAttac_V3
    1. 커브 각도 지정
    2. prefabs 연결
    
    -- 해야할 것 --
    1. Trail 추가하기(Effect)

--- VampireSurvivors/Assets/_Test/_ByeonJinSeong/Script/AttackCurve.cs
+++ VampireSurvivors/Assets/_Test/_ByeonJinSeong/Script/AttackCurve.cs
@@ -5,30 +5,36 @@ using UnityEngine;
 public class AttackCurve : MonoBehaviour
 {
     private float t = 0;
-    public float speed = 10;
+    public float speed = 0.1f;
 
     // 임시 포인터 변수
     Vector2[] point = new Vector2[4];
+    public GameObject MyPigeon;
     public GameObject enemy;
     public float posX = 0.55f;    // x좌표 생성용
     public float posY = 0.45f;    // y좌표 생성용
 
+    bool hit = false;             // 적을 맞췄는지 여부
+    
 
     // Bezier Curve 좌표값 불러오기
     private void Start()
     {
         // Test 나중에 배열로 바꾸기
         // P0 -> 시작 위치
-        point[0] = transform.position;
-        point[1] = RandPoint(transform.position);
-        point[2] = RandPoint(enemy.transform.position); ;
-        point[3] = enemy.transform.position;
+        point[0] = MyPigeon.transform.position;
         // P1 -> 각 1
+        point[1] = RandPoint(transform.position);
         // P2 -> 각 2
+        point[2] = RandPoint(enemy.transform.position); ;
         // P3 -> 도착 위치
+        point[3] = enemy.transform.position;
     }
     void FixedUpdate()
     {
+        if (t > 1) return;
+        if (hit) return;
+
         t += Time.deltaTime * speed;
         AttackTrajectroy();
     }
@@ -56,5 +62,14 @@ public class AttackCurve : MonoBehaviour
         return Mathf.Pow((1 - t), 3) * P0 + Mathf.Pow((1 - t), 2) * 3 * t * P1
             + Mathf.Pow(t, 2) * 3 * (1 - t) * P2
             + Mathf.Pow(t, 3) * P3;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == enemy)
+        {
+            hit = true;
+            Destroy(gameObject,0.1f);
+        }
     }
 }
--- VampireSurvivors/Assets/_Test/_ByeonJinSeong/Script/PigeonAttack.cs
+++ VampireSurvivors/Assets/_Test/_ByeonJinSeong/Script/PigeonAttack.cs
@@ -7,15 +7,49 @@ public class PigeonAttack : MonoBehaviour
     public GameObject AttackBullet;    // 공격 매체
     public GameObject Target;          // Test 테스트(임시) 타겟  
 
-    public int ShootSpeed = 10;
-    void Start()
+    public int Shoot = 10;
+    bool tr = true;
+    //private void Update()
+    //{
+    //    if (tr)
+    //    {
+    //        int _Shoot = Shoot;
+    //        while (_Shoot > 0)
+    //        {
+    //            Debug.Log("생성 while문 입장");
+    //            _Shoot--;
+    //            GameObject attack = Instantiate(AttackBullet, transform);
+    //            attack.GetComponent<AttackCurve>().MyPigeon = gameObject;
+    //            attack.GetComponent<AttackCurve>().enemy = Target;
+    //        }
+    //        tr = false;
+    //    }
+    //}
+
+    private void Start()
     {
-        
+        InvokeRepeating("Attack",2f,1f );
+        //Attack();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Attack()
     {
-        
+        StartCoroutine(CreateObject());
+    }
+
+    IEnumerator CreateObject()
+    {
+        Debug.Log("코루틴 실행");
+        int _Shoot = Shoot;
+        while (_Shoot > 0)
+        {
+            Debug.Log("생성 while문 입장");
+            _Shoot--;
+            GameObject attack = Instantiate(AttackBullet, transform);
+            attack.GetComponent<AttackCurve>().MyPigeon = gameObject;
+            attack.GetComponent<AttackCurve>().enemy = Target;
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
     }
 }
