a444e15e90004000a01af42a1cd3c4b25aca67b1
antcho1024 <50614113+antcho1024@users.noreply.github.com>
2022-04-08 14:50:22 +0900

    Ice 오브젝트 생성

---
+++ VampireSurvivors/Assets/_Test/_ChoHyeSoo/Scripts/Ice.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ice : Item
+{
+    protected override void ActiveAttack(int i)
+    {
+
+    }
+    protected override void WeaponEquipFX()
+    {
+
+    }
+    protected override void Level1() { }
+    protected override void Level2() { }
+    protected override void Level3() { }
+    protected override void Level4() { }
+    protected override void Level5() { }
+    protected override void Level6() { }
+    protected override void Level7() { }
+    protected override void Level8() { }
+}
