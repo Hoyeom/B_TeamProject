0be824f5653bdc2f732d4f9bb284ebb122f24513
hyun233121 <hyunzai95@gmail.com>
2022-04-17 06:55:34 +0900

    성수 완성

--- VampireSurvivors/Assets/_Test/_LeeHyeonjae/Script/Item/HolyWater.cs
+++ VampireSurvivors/Assets/_Test/_LeeHyeonjae/Script/Item/HolyWater.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class HolyWater : Item
 {
 
+    //해야할거
+    
+    //아이템 상세 수치 설명란에 기입 (데미지 등)
+
+    //-------------------------------
+
     /* minMight;  // 최소 공격력
      maxMight;  // 최대 공격력
      coolDown;  // 쿨타임
@@ -18,26 +24,67 @@ public class HolyWater : Item
 
     public GameObject attackPrefab;
     private GameObject tempPrefab;
-    
+
 
     protected override void ActiveAttack(int i)
     {
         tempPrefab = ObjectPooler.Instance.GenerateGameObject(attackPrefab);
-        tempPrefab.transform.position = new Vector2(transform.position.x + 1, 6f); // 초기 위치 지정
+       // tempPrefab.transform.position = new Vector2(transform.position.x + 1, 6f); // 초기 위치 지정
+        tempPrefab.transform.position = new Vector2(transform.position.x + Random.Range(-3f,3f), 6f); // 초기 위치 지정
         tempPrefab.transform.Translate(Vector2.one * Random.Range(1f, .4f)); // 위치 지정
         tempPrefab.transform.rotation = player.viewRotation; // 방향 지정
 
 
-        // ProjectilePrefab stat = tempPrefab.GetComponent<ProjectilePrefab>(); // 발사체 속도 데미지 지정
         HolyWaterPerfab stat = tempPrefab.GetComponent<HolyWaterPerfab>(); // 발사체 속도 데미지 지정
         stat.speed = GetSpeed();
-        stat.amount = GetAmount();
+        stat.might = GetMight();
         stat.area = GetArea();
         stat.rigid.AddForce((Vector2.down * Random.Range(-.2f, .2f)) * speed, ForceMode2D.Impulse);
-        stat.rigid.AddTorque(Random.Range(-90f, 90f));
+        stat.rigid.AddTorque(Random.Range(-360f, 360f));
         //Debug.Log("작동");
 
-
-
     }
+    protected override void Level2()
+    {
+        minMight += 1;
+        maxMight += 1;
+    }
+
+    protected override void Level3()
+    {
+        amount++;
+        minMight += 1;
+        maxMight += 1;
+    }
+
+    protected override void Level4()
+    {
+        coolDown -= 0.5f;
+    }
+
+    protected override void Level5()
+    {
+        area+=0.25f;
+        amount++;
+    }
+
+    protected override void Level6()
+    {
+        amount++;
+    }
+
+    protected override void Level7()
+    {
+        coolDown -= 0.5f;
+        minMight += 1;
+        maxMight += 1;
+    }
+
+    protected override void Level8()
+    {
+        amount++;
+        area += 0.25f;
+       
+    }
+
 }
--- VampireSurvivors/Assets/_Test/_LeeHyeonjae/Script/Item/HolyWaterPerfab.cs
+++ VampireSurvivors/Assets/_Test/_LeeHyeonjae/Script/Item/HolyWaterPerfab.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HolyWaterPerfab : MonoBehaviour
 {
     internal float speed;
-    internal float amount;
+    internal float might;
     internal float area;
     internal Rigidbody2D rigid;
 
@@ -23,37 +23,37 @@ public class HolyWaterPerfab : MonoBehaviour
         GetComponent<SpriteRenderer>().enabled = true;
         mask = LayerMask.GetMask("Enemy");
         coroutine = HolyWaterArea();
-        Invoke("Dead", 1.0f);
+        Invoke("Dead", Random.Range(1f,1.5f));
     }
 
     
-
     void Dead()
     {
         
             HolyWaterZone = transform.position;
             HolyWaterUnit = ObjectPooler.Instance.GenerateGameObject(HolyWaterZones);
+            HolyWaterUnit.transform.localScale = Vector3.one * (area*2);
             HolyWaterUnit.transform.position = HolyWaterZone;
             StartCoroutine(coroutine);
             GetComponent<SpriteRenderer>().enabled = false;
-            Debug.Log("와장창");
+            //Debug.Log("와장창");
         
     }
 
     IEnumerator HolyWaterArea()
     {
-        Collider2D[] cols = Physics2D.OverlapCircleAll(HolyWaterZone, area, mask);
+       // Debug.Log(might);
         for (int j = 0; j < 5; j++)
-        { 
+        {
+            Collider2D[] cols = Physics2D.OverlapCircleAll(HolyWaterZone, area, mask);
             if (cols != null)
             {
 
                 //데미지 주기
                 for (int i = 0; i < cols.Length; i++)
                 {
-                    cols[i].gameObject.GetComponent<Enemy>().HitEnemy(area, transform.position);
-                    //Debug.Log(amount);
-
+                    cols[i].gameObject.GetComponent<Enemy>().HitEnemy(might, transform.position);
+                     
                 }
             }
             yield return new WaitForSeconds(0.3f);
@@ -61,7 +61,7 @@ public class HolyWaterPerfab : MonoBehaviour
       
         ObjectPooler.Instance.DestroyGameObject(this.gameObject);
         ObjectPooler.Instance.DestroyGameObject(HolyWaterUnit);
-        Debug.Log("파괴");
+       // Debug.Log("파괴");
         StopCoroutine(coroutine);
         coroutine = HolyWaterArea();
     }
