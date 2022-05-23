cdeb44c007b6bd589e6b02f28903110c0f5c95d1
tobetop123 <100178815+tobetop123@users.noreply.github.com>
2022-05-23 16:50:42 +0900

    Get여는조건(작업중)

--- VampireSurvivors/Assets/_Test/_LeeHyunSeung/Script/RoomManager.cs
+++ VampireSurvivors/Assets/_Test/_LeeHyunSeung/Script/RoomManager.cs
@@ -7,51 +7,79 @@ public class RoomManager : MonoBehaviour
     public Transform player = null;
     public int stageIndex = 0;
     public int randomStage = 0;
+    
     public GameObject[] stage = null;
     public GameObject[] bossStage = null;
-    private GameObject addObject = null;
+    private GameObject addStage = null;
+
+    public int stageEnemyCount = 0;
+    public int killMonsterCount = 0;
 
     private void Awake()
     {
         // 첫번째 맵
         stageIndex++;
-        addObject = ObjectPooler.Instance.GenerateGameObject(stage[randomStage]);
+        addStage = ObjectPooler.Instance.GenerateGameObject(stage[randomStage]);
+    }
+
+    private void Start()
+    {
+        MonsterCount();
+    }
+
+    private void Update()
+    {
+        if(stageEnemyCount-killMonsterCount == 0)
+        {
+            OnGet();
+        }
     }
 
     public void NextStage()
     {
         stageIndex++;
-        addObject.SetActive(false);
+        addStage.SetActive(false);
         if (stageIndex%5 != 0)
         {
             // 랜덤으로 일반방 이동
             int random = Random.Range(0, stage.Length);
             randomStage = random;
-            addObject = ObjectPooler.Instance.GenerateGameObject(stage[randomStage]);
+            addStage = ObjectPooler.Instance.GenerateGameObject(stage[randomStage]);
             PlayerReposion();
-            Debug.Log($"{addObject}Stage,{stageIndex % 5}");
+            Debug.Log($"{addStage}Stage,{stageIndex % 5}");
             
         }
         else
         {
             // 보스방 순서대로
             Debug.Log($"보스방 입장");
-            addObject = ObjectPooler.Instance.GenerateGameObject(bossStage[(stageIndex / 5) - 1]);
+            addStage = ObjectPooler.Instance.GenerateGameObject(bossStage[(stageIndex / 5) - 1]);
             PlayerReposion();
-            Debug.Log($"{addObject}Stage,{stageIndex}");
+            Debug.Log($"{addStage}Stage,{stageIndex}");
         }
     }
 
     // 케릭터 이동포인트로 이동
     private void PlayerReposion()
     {
-        Transform playerReposion = addObject.transform.Find("PlayerPoint").transform;
+        Transform playerReposion = addStage.transform.Find("PlayerPoint").transform;
         Managers.Game.Player.transform.position = playerReposion.position;
     }
 
-    private void GatOn()
+    private void OnGet()
     {
-        GameObject gate = addObject.GetComponent<Gate>().gameObject;
+        GameObject gate = addStage.GetComponent<Gate>().gameObject;
         gate.SetActive(true);
+        killMonsterCount = 0;
+    }
+
+    private int MonsterCount()
+    {
+        EnemySpawnPoint[] monsterSpwner = addStage.GetComponentsInChildren<EnemySpawnPoint>();
+        for(int i =0; i<monsterSpwner.Length; i++)
+        {
+            stageEnemyCount += monsterSpwner[i].enemyCount;
+        }
+        return stageEnemyCount;
     }
 }
